a1eeda78f6e7a50e705f21bd91aed46f485c4860
DavidMatevosyan21D <113889057+DavidMatevosyan21D@users.noreply.github.com>
2023-04-17 13:46:10 +0300

    Add files via upload

---
+++ userClass/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace userClass
+{
+	public class UserInfo
+	{
+	 public string User { get; set; }
+     public string Pass { get; set; }
+        public string Role { get; set; }
+        public bool IsDeleted { get; set; }
+        public UserInfo(string user, string pass, string role)
+        {
+            User = user;
+            Pass = pass;
+            Role = role;
+
+        }
+        public UserInfo()
+        {
+
+
+        }
+
+        public override string ToString()
+        {
+            return "Пользователь - " + User + "Пароль - " + Pass + "Роль - " + Role + "Удалел ли аккаунт?" + IsDeleted;
+        }
+    }
+}
+
